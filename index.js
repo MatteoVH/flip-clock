10a01cf6f936c87b526c690c9fdc9cb70e1df86a
Matteo Vesprini-Heidrich <matteo.vesprini-heidrich@originate.com>
2018-03-17 16:49:32 -0400

    update time every minute

--- index.js
+++ index.js
@@ -1,27 +1,41 @@
-const time = new Date();
+function updateTime() {
+  const time = new Date();
 
-let ampm = 'am';
+  let ampm = "am";
 
-let hours = time.getHours();
-if (hours >= 12) {
-  ampm = 'pm'
+  let hours = time.getHours();
+  if (hours >= 12) {
+    ampm = "pm";
+  }
+
+  if (hours === 0) {
+    hours = 12;
+  } else if (hours > 12) {
+    hours -= 12;
+  }
+
+  const minutes = time.getMinutes();
+  const minutesString = minutes.toString();
+  if (minutesString.length === 1) {
+    minutesString = "0" + minutesString;
+  }
+
+  const [hoursContainer, minutesContainer] = document.getElementsByClassName(
+    "digit"
+  );
+
+  hoursContainer.innerHTML = hours;
+  minutesContainer.innerHTML = minutesString;
 }
 
-if (hours === 0) {
-  hours = 12;
-} else if (hours > 12) {
-  hours -= 12;
+updateTime();
+
+const timeToMinuteChange =
+  60000 - (new Date().getSeconds() * 1000 + new Date().getMilliseconds());
+
+function updateTimeEveryMinute() {
+  updateTime();
+  setTimeout(updateTimeEveryMinute, 60000);
 }
 
-const minutes = time.getMinutes();
-const minutesString = minutes.toString();
-if (minutesString.length === 1) {
-  minutesString = '0' + minutesString;
-}
-
-const [hoursContainer, minutesContainer] = document.getElementsByClassName('digit');
-
-console.log('hours', hoursContainer)
-
-hoursContainer.innerHTML = hours;
-minutesContainer.innerHTML = minutesString;
+setTimeout(updateTimeEveryMinute, timeToMinuteChange);
